5ce93382f02f7973d9ed853ba207b197149ad7d3
Cindy <syhndie2@gmail.com>
2017-12-31 15:41:01 -0600

    Added RegistrationDateGroup viewmodel for registration statistics table in About view

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,25 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<RegistrationDateGroup> data =
+                from student in _context.Students
+                group student by student.RegistrationDate into dateGroup
+                select new RegistrationDateGroup()
+                {
+                    RegistrationDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
---
+++ ContosoUniversity/Models/SchoolViewModels/RegistrationDateGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+    public class RegistrationDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? RegistrationDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}

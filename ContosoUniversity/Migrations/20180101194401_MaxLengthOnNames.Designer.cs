590fa379581f5bf54ad61002aabca47b059a102f
Cindy <syhndie2@gmail.com>
2018-01-01 16:09:18 -0600

    many things, all from "Creating a com.plex data model" tutorial

--- ContosoUniversity/Migrations/20180101194401_MaxLengthOnNames.Designer.cs
+++ ContosoUniversity/Migrations/20180101194401_MaxLengthOnNames.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180101194401_MaxLengthOnNames")]
+    partial class MaxLengthOnNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -60,12 +61,9 @@ namespace ContosoUniversity.Migrations
                         .ValueGeneratedOnAdd();
 
                     b.Property<string>("FirstMidName")
-                        .IsRequired()
-                        .HasColumnName("FirstName")
                         .HasMaxLength(50);
 
                     b.Property<string>("LastName")
-                        .IsRequired()
                         .HasMaxLength(50);
 
                     b.Property<DateTime>("RegistrationDate");
--- ContosoUniversity/Migrations/20180101204632_ColumnFirstName.Designer.cs
+++ ContosoUniversity/Migrations/20180101204632_ColumnFirstName.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180101204632_ColumnFirstName")]
+    partial class ColumnFirstName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -60,12 +61,10 @@ namespace ContosoUniversity.Migrations
                         .ValueGeneratedOnAdd();
 
                     b.Property<string>("FirstMidName")
-                        .IsRequired()
                         .HasColumnName("FirstName")
                         .HasMaxLength(50);
 
                     b.Property<string>("LastName")
-                        .IsRequired()
                         .HasMaxLength(50);
 
                     b.Property<DateTime>("RegistrationDate");

93910f6c19c3e229ec75c1531c898e0ea80957bb
Cindy <syhndie2@gmail.com>
2018-01-15 17:03:27 -0600

    Added Person class. Student and Instructor both inherit from Person. Created migration to reflect this new relationship.

---
+++ ContosoUniversity/Migrations/20180115221558_Inheritance.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class Inheritance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Student_StudentID",
+                table: "Enrollment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Enrollment_StudentID",
+                table: "Enrollment");
+
+            migrationBuilder.RenameTable(
+                name: "Instructor", newName: "Person");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EnrollmentDate",
+                table: "Person",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Person",
+                nullable: false,
+                maxLength: 128,
+                defaultValue: "Instructor");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "HireDate",
+                table: "Person",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OldId", table: "Person", nullable: true);
+
+            //Copy existing Student data into new Person table.
+            migrationBuilder.Sql("INSERT INTO dbo.Person (LastName, FirstName, HireDate, EnrollmentDate, Discriminator, OLDId) SELECT LastName, FirstName, null AS HireDate, EnrollmentDate, 'Student' AS Discriminator, ID as OldId FROM dbo.Student");
+
+            //Fix up existing relationships to match new Primary Keys.
+            migrationBuilder.Sql("UPDATE dbo.Enrollment SET StudentID = (SELECT ID FROM dbo.Person WHERE OldId = Enrollment.StudentID AND Discriminator = 'Student')");
+
+            //Remove temporary key
+            migrationBuilder.DropColumn(
+                name: "OldId",
+                table: "Person");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollment_StudentID",
+                table: "Enrollment",
+                column: "StudentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Person_StudentID",
+                table: "Enrollment",
+                column: "StudentID",
+                principalTable: "Person",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}

c4688dc6a8157140fd06d1ed0ad4356d53709414
Cindy <syhndie2@gmail.com>
2018-01-16 16:06:59 -0600

    Added down migration code (taken from https://github.com/aspnet/Docs/blob/master/aspnetcore/data/ef-mvc/intro/samples/cu-final/Migrations/20170816195930_Inheritance.cs) to Inheritance migration

--- ContosoUniversity/Migrations/20180115221558_Inheritance.cs
+++ ContosoUniversity/Migrations/20180115221558_Inheritance.cs
@@ -71,7 +71,97 @@ namespace ContosoUniversity.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseAssignment_Person_InstructorID",
+                table: "CourseAssignment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Person_InstructorID",
+                table: "Department");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Person_StudentID",
+                table: "Enrollment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OfficeAssignment_Person_InstructorID",
+                table: "OfficeAssignment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Person",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Person");
+
+            migrationBuilder.RenameTable(
+                name: "Person",
+                newName: "Student");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EnrollmentDate",
+                table: "Student",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Student",
+                table: "Student",
+                column: "ID");
+
+            migrationBuilder.CreateTable(
+                name: "Instructor",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    HireDate = table.Column<DateTime>(nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instructor", x => x.ID);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseAssignment_Instructor_InstructorID",
+                table: "CourseAssignment",
+                column: "InstructorID",
+                principalTable: "Instructor",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Instructor_InstructorID",
+                table: "Department",
+                column: "InstructorID",
+                principalTable: "Instructor",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Student_StudentID",
+                table: "Enrollment",
+                column: "StudentID",
+                principalTable: "Student",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OfficeAssignment_Instructor_InstructorID",
+                table: "OfficeAssignment",
+                column: "InstructorID",
+                principalTable: "Instructor",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
         }
     }
 }
